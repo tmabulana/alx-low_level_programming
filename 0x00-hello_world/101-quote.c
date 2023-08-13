ee2f50d0b5793981e3fcdc63adff0ef98df281d5
tmabulana <mabulanathabang31@gmail.com>
2023-08-13 09:48:03 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistdio.h> 
+/**
+ * main - must print a line
+ *
+ * Return: always 1
+ *
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
