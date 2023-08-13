e617b29f14eb33853923718f2b8cf6d0854d3c2d
tmabulana <mabulanathabang31@gmail.com>
2023-08-13 05:20:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main this is
+ *
+ * Return: 0 always
+ *
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
