f588dfec43e2436989debb75bc6ecba58d599bd7
tmabulana <mabulanathabang31@gmail.com>
2023-08-13 01:51:43 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function to be used is this one
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
