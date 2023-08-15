37e1e88411105fd0c97d5dc279b67c02caf94443
tmabulana <mabulanathabang31@gmail.com>
2023-08-14 20:09:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - main function this is
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar(i);
+for (i = 'A' ; i <= 'Z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+
+}
