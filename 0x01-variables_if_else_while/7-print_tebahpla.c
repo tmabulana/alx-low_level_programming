16109048c0f8e5a8fae9c9e5f1b0627d6e00c709
tmabulana <mabulanathabang31@gmail.com>
2023-08-14 20:52:42 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - fucntion main this is
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+char i;
+for (i = 'z'; i >= 'a' ; i==)
+putchar(i);
+putchar('\n');
+return (0);
+}
