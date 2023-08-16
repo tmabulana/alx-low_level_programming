a857ee31a9a3b15e057094d8b86f976a5998074e
tmabulana <mabulanathabang31@gmail.com>
2023-08-16 15:50:25 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,7 @@
 void print_times_table(int n);
 {
 int (num), (mult), (prod),
+}
 {
 if (n >= 0 && n <= 15)
 {
@@ -35,7 +36,6 @@ _putchar((prod / 10) + '0');
 _putchar((prod % 10) + '0');
 }
 _putchar('\n');
-}
 }
 }
 }
