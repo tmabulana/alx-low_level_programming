5f607158b411b3640ab5ff6142255f4549f2071b
tmabulana <mabulanathabang31@gmail.com>
2023-08-15 20:32:41 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - function to find the absolute value of a number only
+ * @a: function parameter
+ * Return: -a or a;
+ */
+
+int _abs(int a)
+{
+if (a < 0)
+return (-a);
+else if (a >= 0)
+{
+return (a);
+}
+return (0);
+}
+
