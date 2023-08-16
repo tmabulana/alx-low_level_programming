3e659208e4cc7ad53fc331db07d0f206dcd19fed
tmabulana <mabulanathabang31@gmail.com>
2023-08-15 21:59:59 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include "main.c"
+#include <stdio.h>
+/**
+ * time_table - print the time accurately
+ * Return: numbers
+ */
+void times_table(void)
+{
+int i, j, result;
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j < 10 ; j++)
+{
+result = i * j;
+if (j == 0)
+printf("%d, ", result);
+else
+{
+printf("%2d", result);
+if (j != 9)
+	printf(", ");
+}
+printf("%2d, ", result);
+}
+printf("\n");
+}
+printf("\n");
+}
