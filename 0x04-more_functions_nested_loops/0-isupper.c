ad6d0f97b7ecd222b2216e7ba8c083e12c7db0ea
tmabulana <mabulanathabang31@gmail.com>
2023-08-17 14:55:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - has to check upperclass character always
+ * @c: parameter has to always be checked
+ * Return: 1 always on success and 0 on failure
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c >= 'Z')
+return (1);
+else
+return (0);
+}
