d40db5221c3a11d5472db0afce6db96cf2d2c79f
tmabulana <mabulanathabang31@gmail.com>
2023-08-17 15:52:15 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * more numbers - prints numbers from 0 to 14 only and always
+ * Return: void
+ */
+void more_numbers(void);
+{
+int i;
+int j;
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j <= 14 ; j++)
+{
+_putchar(j + '0');
+}
+_putchar('\n');
+}
+}
