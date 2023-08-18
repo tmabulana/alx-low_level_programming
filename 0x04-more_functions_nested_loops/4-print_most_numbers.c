1fb24eaea8e3bd68ddcfe926a24fb1a5d4a0bcac
tmabulana <mabulanathabang31@gmail.com>
2023-08-18 16:59:20 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,13 @@
  */
 void print_most_numbers(void)
 {
-int 1;
-for (i = 0 ; i < 10 ; i++)
+int i = 0;
+for (; i <= 9; i++)
 {
-if (i != 2 && i != 4)
+if (i == 2 || i == 4)
+{
 _putchar(i + '0');
+}
 }
 _putchar('\n');
 }
