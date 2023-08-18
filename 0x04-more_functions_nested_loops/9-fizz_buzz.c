abf09d149057997133461d4e057310e49e41fcd2
tmabulana <mabulanathabang31@gmail.com>
2023-08-17 19:47:25 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - this is the main function to be exact
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 1 ; i <= 100 ; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+printf("%s", "FizzBuzz");
+else if (i % 3 == 0)
+printf("%s", "Fizz");
+else if (i % 5 == 0)
+printf("%s", "Buzz");
+else
+printf("%d", i);
+if (i != 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
