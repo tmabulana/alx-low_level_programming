fdf755ac39ec42de7854cbee62e66d1163f18290
tmabulana <mabulanathabang31@gmail.com>
2023-08-18 17:43:43 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * print_square - a square will be printed using the character '#'
+ * print_square - a square will be printed a number of times
  * @size: size of the square
  * Return: void
  */
 
 void print_square(int size);
 {
+int i, j;
 if (size <= 0)
+{
 _pucthar('\n')
+}
 else
 {
-int i, j;
-for (i = 1 ; i <= size ; i++)
+for (i = 0; i < size; i++)
 {
-for (j = 1 ; j <= size ; j++)
-_putchar('#');
+for (j = 0; j < size; j++)
+{
+_putchar(35);
+}
 _putchar('\n');
 }
 }
