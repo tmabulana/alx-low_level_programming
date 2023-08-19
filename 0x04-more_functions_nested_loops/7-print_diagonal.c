d3d8edfdc9fd09ae6f3b5a296f5996f1eead4c4c
tmabulana <mabulanathabang31@gmail.com>
2023-08-18 17:29:53 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
-* print_diagonal - diagonal has to be printed with the use of '\' character
-* @n: the '\' has to be printed several times
+* print_diagonal - diagonal has to be printed with regards to the parameter
+* @n: the diagonal line has to be printed several times
 * Return: void
 */
 
 void print_diagonal(int n)
 {
+int n, j;
 if (n <= 0)
 _putchar('\n');
 else
 {
-int i, j;
-for (i = 0 ; i < n ; i++)
+for (i = 0; i < n; i++)
 {
-for (j = 0 ; j <= i ; j++)
-_putchar(' ');
-_putchar('\\');
+for (j = 0; j < i; j++)
+{
+_putchar(32);
+}
+_putchar(92);
 _putchar('\n');
 }
 }
