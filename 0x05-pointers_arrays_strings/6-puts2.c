f9b8122e399178e6b0383bf21b7c6db267a0f018
tmabulana <mabulanathabang31@gmail.com>
2023-08-21 21:24:02 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,7 +10,7 @@ void puts2(char *str)
 int longi = 0;
 int t = 0;
 char *y = str;
-int 0;
+int o;
 while (*y != '\0')
 {
 y++;
@@ -21,8 +21,8 @@ for (o = 0 ; o <= t ; o++)
 {
 if (o % 2 == 0)
 {
-_putchar(str[o]);
+putchar(str[o]);
 }
 }
-_putchar('\n')
+putchar('\n');
 }
