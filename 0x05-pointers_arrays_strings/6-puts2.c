0cbba0bfc2fcfc4969f186334ddfb3eb8ed8530a
tmabulana <mabulanathabang31@gmail.com>
2023-08-21 20:47:07 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts2 - function should print only a single character out of two only
+ * starting eith the first one only
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+int longi = 0;
+int t = 0;
+char *y = str;
+int 0;
+while (*y != '\0')
+{
+y++;
+longi++;
+}
+t = longi - 1;
+for (o = 0 ; o <= t ; o++)
+{
+if (o % 2 == 0)
+{
+_putchar(str[o]);
+}
+}
+_putchar('\n')
+}
