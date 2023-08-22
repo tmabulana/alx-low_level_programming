df4582313ce147931aa203e28f707fb8c84d248c
tmabulana <mabulanathabang31@gmail.com>
2023-08-21 20:13:09 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string that is followed by stdout, a new line
+ * @str: string that is printed
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
