e60ba9a88c1a7d845225449fd9e5995f7ad2f904
tmabulana <mabulanathabang31@gmail.com>
2023-08-22 22:49:09 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,7 +9,7 @@ int main(void)
 {
 char password[84];
 int index = 0, sum = 0, diff_half1, diff_half2;
-srand(time(0));
+srand(time(NULL));
 while (sum < 2772)
 {
 password[index] = 33 + rand() % 94;
