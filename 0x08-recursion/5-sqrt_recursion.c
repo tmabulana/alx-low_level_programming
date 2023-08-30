5404cde065cea0c548e1f227a797d09d2d71309e
tmabulana <mabulanathabang31@gmail.com>
2023-08-30 13:07:50 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,8 +2,8 @@
 /**
  * sqrt_a - will return the square root of
  * a number
- * @n: input number
- * @c: the iterator
+ * @a: input number
+ * @b: the iterator
  * Return: the square root of -1
  */
 int sqrt_a(int a, int b)
