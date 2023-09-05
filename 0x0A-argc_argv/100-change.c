7025bc2aba135f073459643b878cfc6eed0e7cd6
tmabulana <mabulanathabang31@gmail.com>
2023-09-04 17:28:28 -0700

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,11 +20,11 @@ while (cents > 0)
 {
 if (cents >= 25)
 cents -= 25;
-if (cents >= 10)
+else if (cents >= 10)
 cents -= 10;
-if (cents >= 2)
+else if (cents >= 2)
 cents -= 2;
-if (cents >= 1)
+else if (cents >= 1)
 cents -= 1;
 mncoin += 1;
 }
