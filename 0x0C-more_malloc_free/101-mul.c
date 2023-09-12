8a16b1c826e28824a726ad9d3c9703042a033d40
tmabulana <mabulanathabang31@gmail.com>
2023-09-11 18:57:14 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -33,7 +33,7 @@ i++;
 return (i);
 }
 /**
- * error - will handle the main errors
+ * errors - will handle the main errors
  */
 void errors(void)
 {
@@ -52,7 +52,7 @@ char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-error();
+errors();
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
@@ -63,7 +63,7 @@ for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 {
-digit1 = s1[len] - '0';
+digit1 = s1[len1] - '0';
 carry = 0;
 for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
@@ -72,7 +72,7 @@ carry += result[len1 + len2 + 1] = (digit1 *digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 }
-if (carry > 10)
+if (carry > 0)
 result[len1 + len2 + 1] += carry;
 }
 for (i = 0; i < len - 1; i++)
