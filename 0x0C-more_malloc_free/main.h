b74c2c555bced1c22cdcb5cc4cedbdc4b7b8d47c
tmabulana <mabulanathabang31@gmail.com>
2023-09-11 18:20:54 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
+int main(int argc, char *argv[]);
+int _strlen(char *s);
+int is_digit(char *s);
+void error(void);
 
 #endif
