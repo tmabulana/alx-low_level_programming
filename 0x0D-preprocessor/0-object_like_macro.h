9ca223405afcb05eb1045308da9e9843be4f658a
tmabulana <mabulanathabang31@gmail.com>
2023-09-11 19:42:02 -0700

    0. Object-like Macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * File: 0-object_like_macro
+ * Desc: the header file
+ */
+#define SIZE 1024
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This is the Readme file, for 0x0D. C - Preprocessor
