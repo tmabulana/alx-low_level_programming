d86ed5790a29e8e372d18e246e034e27d91dcaa9
tmabulana <mabulanathabang31@gmail.com>
2023-09-13 08:03:09 -0700

    3. Function-like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef MACROLIKE_H
+#define MACROLIKE_H
+/**
+ * File: 3-function_like_macro
+ * @x: the input value (an int)
+ * Desc: the header file that computes the exact value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+#endif
