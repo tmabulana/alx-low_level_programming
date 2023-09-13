ee1fd13f7bf88c34acb72e226d8fc14e8c9c9f56
tmabulana <mabulanathabang31@gmail.com>
2023-09-13 15:13:31 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a Redame file, for 0x0E-structures_typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - this is the struct that creates the dog info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the ownwer of the dog
+ * Description: firts struct with ALX
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
