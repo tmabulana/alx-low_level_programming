fbd29b98747e363d4c00a86f0e3f1772befb7b92
tmabulana <mabulanathabang31@gmail.com>
2023-09-13 07:52:34 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - is to printout a file name of a source code
+ * Return: int
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
