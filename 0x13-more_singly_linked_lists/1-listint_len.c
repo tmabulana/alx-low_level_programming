f7d09d1a613d84c2a1acde31d0e9b3c3ac5e51ee
tmabulana <mabulanathabang31@gmail.com>
2023-09-25 17:33:53 -0700

    1. List length.

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listin_len - will count the number of elements in a list
+ * @h: the head pointer
+ * Return: the number of element in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+size_t count;
+if (h == NULL)
+return (0);
+for (count = 0; h != NULL; count++)
+{
+h = h->next;
+}
+return (count);
+}
