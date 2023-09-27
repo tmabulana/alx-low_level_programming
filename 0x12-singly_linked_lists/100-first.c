b07f78c8e32cf77b5d6be4aae9a598805734a86f
tmabulana <mabulanathabang31@gmail.com>
2023-09-27 09:15:57 -0700

    5. The Hare and the Tortoise.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void ComeFirstbyTmabulana(void)__attribute__ ((constructor));
+/**
+ * ComeFirst - this is the function that will run before
+ * the main function
+ */
+void ComeFirstbyTmabulana(void)
+{
+printf("You're beat! and yet, you muat allow,\n");
+printf("I bore my house upon my back!\n");
+}
