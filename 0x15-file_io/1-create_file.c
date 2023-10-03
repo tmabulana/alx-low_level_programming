8191ef823899f734840cb9d5cbb6376ea6e814f8
tmabulana <mabulanathabang31@gmail.com>
2023-10-03 03:36:42 -0700

    1. Under the snow.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int nletters;
 int rwr;
 if (!filename)
 return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 if (text_content == NULL)
